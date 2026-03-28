3ceebdd1add634a877566462ce0f788e01140d8c
170joeychua <170joeychua@gmail.com>
2026-03-29 00:13:54 +0800

    Init project!!!!!!!!!!!!!!!!!!!!!!

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettier = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      prettierConfig, // disables conflicting ESLint rules
+    ],
+    processor: angular.processInlineTemplates,
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error', // ✅ enforce prettier
+
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {
+      // rely on the Angular template rules instead of Prettier
+    },
+  },
+]);
