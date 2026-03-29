164a5bdd95db70536464633bd130fda90e754d22
170joeychua <170joeychua@gmail.com>
2026-03-29 22:18:41 +0800

    Chore: Format lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-// @ts-check
 const eslint = require('@eslint/js');
 const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
@@ -9,6 +8,7 @@ const prettierConfig = require('eslint-config-prettier');
 module.exports = defineConfig([
   {
     files: ['**/*.ts'],
+    ignores: ['.angular/cache/**', 'dist/**', 'node_modules/**'],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
@@ -22,33 +22,22 @@ module.exports = defineConfig([
     },
     rules: {
       'prettier/prettier': 'error', // ✅ enforce prettier
-
       '@angular-eslint/directive-selector': [
         'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
+        { type: 'attribute', prefix: 'app', style: 'camelCase' },
       ],
       '@angular-eslint/component-selector': [
         'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
+        { type: 'element', prefix: 'app', style: 'kebab-case' },
       ],
+      '@typescript-eslint/no-useless-constructor': 'off',
+      'no-useless-constructor': 'off',
     },
   },
   {
     files: ['**/*.html'],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {
-      // rely on the Angular template rules instead of Prettier
-    },
+    ignores: ['.angular/cache/**', 'dist/**', 'node_modules/**'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
   },
 ]);
