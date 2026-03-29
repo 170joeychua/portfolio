4d04c287c6a327a0d90e87d575d026efcc26a857
170joeychua <170joeychua@gmail.com>
2026-03-29 20:05:32 +0800

    Footer: Init footer skeleton. - Add icon buttons and are clickable

---
+++ src/app/bottom-footer/bottom-footer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-footer',
+  standalone: true,
+  templateUrl: './bottom-footer.html',
+})
+export class BottomFooterComponent {
+}
